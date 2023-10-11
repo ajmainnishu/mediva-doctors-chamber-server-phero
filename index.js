92b8f1e5f4482f8029f3212ebfccac9b219eba1e
Ajmain Nishu <ajmainnishu@gmail.com>
2023-10-11 23:40:32 +0600

    feat: doctors get and post added

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
+
+// mongodb link
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ffrq.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const doctorsCollection = client.db('usersDB').collection('doctors');
+    // doctors get data
+    app.get('/doctors', async (req, res) => {
+      const result = await doctorsCollection.find().toArray();
+      res.send(result);
+    })
+    // doctors post data
+    app.post('/doctors', async (req, res) => {
+      const data = req.body;
+      const doc = {
+        img: data.photo,
+        name: data.name,
+        place: data.place,
+        time: data.time,
+        Price: data.price,
+        expert: data.expert
+      }
+      const result = await doctorsCollection.insertOne(doc);
+      res.send(result);
+    })
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Mediva Doctors Chamber Running')
